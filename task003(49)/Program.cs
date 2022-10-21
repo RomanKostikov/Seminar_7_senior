d9b9f11e002d9f6c90dce6f4bccdcb1669d37d2a
Roman <romankos1993@gmail.com>
2022-10-21 22:24:17 +0300

    task 003(49) conpleted

---
+++ task003(49)/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, 
+// у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+void Main()
+{
+    Console.Clear();
+    Console.Write("Введите количество строк: ");
+    int rows = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите количество столбцов: ");
+    int columns = int.Parse(Console.ReadLine()!);
+    int[,] array = NewArray(rows, columns);
+    PrintArray(array);
+    int[,] newArray = SquadArray(array);
+    Console.WriteLine();
+    PrintArray(newArray);
+}
+
+int[,] NewArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return result;
+}
+
+int[,] SquadArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                arr[i, j] *= arr[i, j];
+            }
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Main();
